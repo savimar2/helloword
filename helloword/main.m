2667bcf2ebdd6ca9ce30560ce0d1cd000d5a48f8
Admin <admin@Mac-Admin.local>
2015-07-29 14:52:14 +0300

    Initial Commit

---
+++ helloword/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helloword
+//
+//  Created by Admin on 29.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
